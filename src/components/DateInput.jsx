60796b3a0b2d77500f8c15f1ba5a149b213888db
Sergei Cherniaev <sergei@cherniaev.com>
2020-02-07 16:09:51 +0300

    added baseprovider with lighttheme

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -1,18 +1,21 @@
 import * as React from "react";
+import { LightTheme, BaseProvider } from "baseui";
 import { Datepicker } from "baseui/datepicker";
 import ru from "date-fns/locale/ru";
 
 export default ({ onDateChange }) => {
   const [singleDate, setSingleDate] = React.useState(Date.now());
   return (
-    <Datepicker
-      locale={ru}
-      formatString={"dd.MM.yyyy"}
-      value={singleDate}
-      onChange={({ date }) => {
-        setSingleDate(date);
-        onDateChange(date);
-      }}
-    />
+    <BaseProvider theme={LightTheme}>
+      <Datepicker
+        locale={ru}
+        formatString={"dd.MM.yyyy"}
+        value={singleDate}
+        onChange={({ date }) => {
+          setSingleDate(date);
+          onDateChange(date);
+        }}
+      />
+    </BaseProvider>
   );
 };
